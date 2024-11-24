1000dc753cad4a2fbe10218b4ba76852056f57a7
angel zambrano <angeldzambrano99@gmail.com>
2024-11-24 08:43:50 -0800

    c

--- project/app/page.js
+++ project/app/page.js
@@ -1,24 +1,21 @@
 "use client"
 import Image from "next/image";
-import UserLocation from "./components/UserLocation";
 
 const WIDTH = "1920"
 const HEIGHT = "1080"
 
-const FIRST_MIC = [0.2, 0]
-const SECOND_MIC = [0.8, 0]
+import RectangleMap from './RectangleMap';
 
 export default function Home() {
+  const objects = [
+    { type: 'user', x: 0.125, y: 0.25, size: 0.025 },   // A blue user (x: 12.5%, y: 25%, size: 2.5% of container width)
+    { type: 'object', x: 0.5, y: 0.5, size: 0.0375 }, // A red object (x: 37.5%, y: 50%, size: 3.75% of container width)
+    { type: 'user', x: 0.625, y: 0.333, size: 0.03125 }, // Another blue user (x: 62.5%, y: 33.3%, size: 3.125% of container width)
+  ];
+
   return (
-    <div className="inset-0 fixed bg-red-500">
-      <div>
-        <div style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-        }}>first mic</div>
-        <div>Second mic</div>
-      </div>
-    </div>
+    <main className="bg-gray-100 min-h-screen">
+      <RectangleMap objects={objects} />
+    </main>
   );
 }
